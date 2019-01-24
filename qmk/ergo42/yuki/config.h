f6aa23cc934ff685500d923c094e1b3b4667a742
yukixiii <blackcatxiii.fate@gmail.com>
2019-01-24 20:55:31 +0900

    Update mousekeys configuration.

--- qmk/ergo42/yuki/config.h
+++ qmk/ergo42/yuki/config.h
@@ -33,8 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 /* Mousekeys configuration */
-#define MOUSEKEY_DELAY             50
-#define MOUSEKEY_INTERVAL          40
+#define MOUSEKEY_DELAY             60
+#define MOUSEKEY_INTERVAL          50
 #define MOUSEKEY_MAX_SPEED         20
 #define MOUSEKEY_TIME_TO_MAX       10
 #define MOUSEKEY_WHEEL_MAX_SPEED   20
