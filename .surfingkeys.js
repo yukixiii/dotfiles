ebaa7a216e1ecdabd543dc5a4df16f6642f004f5
yukixiii <blackcatxiii.fate@gmail.com>
2020-01-30 11:50:36 +0900

    Add surfingkeys settings.

---
+++ .surfingkeys.js
@@ -0,0 +1,11 @@
+// file:///C:/Users/yuki/Documents/.surfingkeys.js
+// file:///home/yuki/.surfingkeys.js
+
+unmap("r");
+unmap("<Ctrl-6>");
+
+settings.hintAlign = "left";
+
+mapkey('gl', '#12Open Chrome Settings', function() {
+    tabOpenLink("chrome://settings/");
+});
